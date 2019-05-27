2ccddea078f713d72945fd8ce9294e0222cecb1d
Kronopt <Kronopt@users.noreply.github.com>
2019-05-27 10:44:35 +0100

    Update top_10_DEGs.py
    Now also generates 2 lists of top 10 genes for both comparisons (1vs5 VS 3vs7 and 1vs3 VS 5vs7) where each gene is only differentially expressed in that specific comparison

--- CoffeOmics/top_10_DEGs.py
+++ CoffeOmics/top_10_DEGs.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+"""
+Generates a table of the top 10 DEG's UP- and DOWN-regulated for each comparison.
+Also generates the top 10 DEG's UP- and DOWN-regulated for 1v5 VS 3v7 and 1v3 VS 5v7 specific to each comparison
+(top 10 DEG's of 1v5 not in 3v7 and vice versa, same for 1v3 VS 5v7)
+"""
+
 import argparse
 import csv
 import os
@@ -59,9 +65,14 @@ if __name__ == '__main__':
 
         # top10_DEGs.csv
         print('Generating top10_DEGs.csv ... ', end='')
+        ordered_de = {}
         for de_results_name in de_results_dict:
-            top_10_de_up_list = sorted(de_results_dict[de_results_name]['up'].items(), key=lambda z: z[1], reverse=True)[:10]
-            top_10_de_down_list = sorted(de_results_dict[de_results_name]['down'].items(), key=lambda z: z[1], reverse=True)[:10]
+            ordered_de[de_results_name] = {}
+            ordered_de[de_results_name]['up'] = sorted(de_results_dict[de_results_name]['up'].items(), key=lambda z: z[1], reverse=True)
+            ordered_de[de_results_name]['down'] = sorted(de_results_dict[de_results_name]['down'].items(), key=lambda z: z[1], reverse=True)
+
+            top_10_de_up_list = ordered_de[de_results_name]['up'][:10]
+            top_10_de_down_list = ordered_de[de_results_name]['down'][:10]
 
             with open(os.path.join(parser.output_folder, de_results_name + '_top10_DEGs.csv'), 'w', newline='') as top10_csv:
                 values_csv_writer = csv.writer(top10_csv)
@@ -84,6 +95,55 @@ if __name__ == '__main__':
                     except KeyError:
                         genes_description = ''
                     values_csv_writer.writerow([gene_name, genes_description, foldchange])
+        print('OK')
+
+        # top10_DEGs_comparison_specific.csv
+        print('Generating top10_DEGs_comparison_specific.csv ... ', end='')
+        comparions_to_do = {'1vs5': '3vs7', '3vs7': '1vs5', '1vs3': '5vs7', '5vs7': '1vs3'}
+        comparisons_names = {'1vs5': '1vs5 VS 3vs7', '3vs7': '1vs5 VS 3vs7', '1vs3': '1vs3 vs 5vs7', '5vs7': '1vs3 vs 5vs7'}
+        comparisons = {'1vs5 VS 3vs7': {
+                          'up': {'1vs5': [], '3vs7': []},
+                          'down': {'1vs5': [], '3vs7': []}
+                      },'1vs3 vs 5vs7': {
+                          'up': {'1vs3': [], '5vs7': []},
+                          'down': {'1vs3': [], '5vs7': []}}}
+        
+        # For each comparison (1vs5, 3vs7, 1vs3, 5vs7) check the top genes that are not in the other comparison of the same up/down regulated set.
+        # Do this until 10 genes are found
+        for de_results_name in de_results_dict:
+            gene_count = 10
+            for gene_name, foldchange in ordered_de[de_results_name]['up']:
+                if gene_name not in de_results_dict[comparions_to_do[de_results_name]]['up']:
+                    try:
+                        genes_description = seqtable_dict[gene_name]
+                    except KeyError:
+                        genes_description = ''
+                    comparisons[comparisons_names[de_results_name]]['up'][de_results_name].append((gene_name, genes_description, foldchange))
+                    gene_count -= 1
+                    if gene_count == 0:
+                        break
+            gene_count = 10
+            for gene_name, foldchange in ordered_de[de_results_name]['down']:
+                if gene_name not in de_results_dict[comparions_to_do[de_results_name]]['down']:
+                    try:
+                        genes_description = seqtable_dict[gene_name]
+                    except KeyError:
+                        genes_description = ''
+                    comparisons[comparisons_names[de_results_name]]['down'][de_results_name].append((gene_name, genes_description, foldchange))
+                    gene_count -= 1
+                    if gene_count == 0:
+                        break
+        for comparison_of_comparisons in comparisons:
+            with open(os.path.join(parser.output_folder, comparison_of_comparisons.replace(' ', '_') + '_top10_DEGs.csv'), 'w', newline='') as top10_csv:
+                values_csv_writer = csv.writer(top10_csv)
+                values_csv_writer.writerow(['UP-regulated'])  # sub-header
+                for comp in comparisons[comparison_of_comparisons]['up']:
+                    values_csv_writer.writerow([comp + ' only'])  # sub-header 2
+                    values_csv_writer.writerows(comparisons[comparison_of_comparisons]['up'][comp])
+                values_csv_writer.writerow(['DOWN-regulated'])  # sub-header
+                for comp in comparisons[comparison_of_comparisons]['down']:
+                    values_csv_writer.writerow([comp + ' only'])  # sub-header 2
+                    values_csv_writer.writerows(comparisons[comparison_of_comparisons]['down'][comp])
         print('OK')
 
     else:
